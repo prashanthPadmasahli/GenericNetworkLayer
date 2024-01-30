94afccbaaa4cff6e93a52f65343a41a713b8a72d
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-01-30 20:39:07 +0530

    Initial Commit

---
+++ NetworkLayer_Jan2024/NetworkLayer_Jan2024App.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayer_Jan2024App.swift
+//  NetworkLayer_Jan2024
+//
+//  Created by mac on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayer_Jan2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
